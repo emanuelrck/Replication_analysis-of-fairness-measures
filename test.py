a0781d74dc23b99e7e3d3de566be119a33a229ca
alessandro <vita02051802@gmail.com>
2022-10-25 18:33:19 +0200

    added test assuring that in each row n is consistent

--- test.py
+++ test.py
@@ -1,4 +1,18 @@
-def main():
-    return "Hello World!"
+import glob
+import re
+import numpy as np
+import pickle
 
-assert main() == "Hello World!"
+def binData2array(fname):
+    fname
+    with open(fname, 'rb') as f:
+        X = pickle.load(f)
+    return X
+
+# Test if each row sums to `n`
+for f in glob.glob('*'):
+    if (re.match(r'n.*\.bin$', f)):
+        n = int(f.strip('n').strip('.bin'))
+        x = binData2array(f)
+        for row in x:
+            assert np.sum(row) == n
